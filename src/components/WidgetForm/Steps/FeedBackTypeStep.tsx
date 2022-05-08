c2f06d8f79a149d67c0279ef68ca1688ab9236ad
Rafael Silveira <rafaelsilveira2011@live.com>
2022-05-07 23:33:23 -0300

    Segunda aula completa, front

--- src/components/WidgetForm/Steps/FeedBackTypeStep.tsx
+++ src/components/WidgetForm/Steps/FeedBackTypeStep.tsx
@@ -1,27 +1,36 @@
-import { feedbackType, feedbackTypes } from "..";
+import { FeedbackType, feedbackTypes } from "..";
+import { CloseButton } from "../../CloseButton";
  
 interface FeedbackTypeStepProps{
-    onFeedbackTypeChanged: (type: feedbackType) => void;
+    onFeedbackTypeChanged: (type: FeedbackType) => void;
 }
 export function FeedbackTypeStep(props: FeedbackTypeStepProps){
     return(
-        <div className="flex py-8 gap-2 w-full">
-        {Object.entries(feedbackTypes).map(([key, value]) => {
-            return(
-                <button
-                    key={key}
-                    className=" bg-zinc-800 rounded-lg py-5 w-24 
-                    flex-1 flex flex-col items-center gap-2
-                    border-2 border-transparent 
-                    hover:border-brand-500 focus:border-brand-500 focus:outline-none" 
-                    onClick={() => props.onFeedbackTypeChanged(key as feedbackType)}  // key as feedbackType fala que a chave tem que ser obrigatoria tipo feedbackType por causa do typescript
-                    type="button"
-                >
-                    <img src={value.image.source} alt={value.image.alt} />
-                    <span>{value.title}</span>
-                </button>
-            );
-        }) }
-    </div> 
+        <>
+            <header>
+                <span className="text-xl leading-6"> Deixe seu Feedback</span>
+
+                <CloseButton/>
+            </header>
+
+            <div className="flex py-8 gap-2 w-full">
+            {Object.entries(feedbackTypes).map(([key, value]) => {
+                return(
+                    <button
+                        key={key}
+                        className=" bg-zinc-800 rounded-lg py-5 w-24 
+                        flex-1 flex flex-col items-center gap-2
+                        border-2 border-transparent 
+                        hover:border-brand-500 focus:border-brand-500 focus:outline-none" 
+                        onClick={() => props.onFeedbackTypeChanged(key as FeedbackType)}  // key as feedbackType fala que a chave tem que ser obrigatoria tipo feedbackType por causa do typescript
+                        type="button"
+                    >
+                        <img src={value.image.source} alt={value.image.alt} />
+                        <span>{value.title}</span>
+                    </button>
+                );
+            }) }
+            </div> 
+     </> 
     )
 }
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -5,7 +5,8 @@ import bugImageUrl from '../../assets/bug.svg'
 import ideaImageUrl  from '../../assets/idea.svg'
 import thoughtImageUrl from '../../assets/thought.svg'
 import { FeedbackTypeStep } from "./Steps/FeedBackTypeStep";
-import { FeedbackContentStep } from "./Steps/FeedBacktContentStep";
+import { FeedbackContentStep } from "./Steps/FeedBackContentStep";
+import { FeedBackSuccessStep } from "./Steps/FeedBackSuccessStep";
 
 
 export const feedbackTypes = {
@@ -32,26 +33,36 @@ export const feedbackTypes = {
     },
 }
 
-export type feedbackType =  keyof typeof feedbackTypes
+export type FeedbackType =  keyof typeof feedbackTypes // tipo que so poder ser os titulos de cima
 
 export function WidgetForm(){
-    const [feedbackType, setFeedbackType ] = useState<feedbackType | null>(null)
+    const [feedbackType, setFeedbackType ] = useState<FeedbackType | null>(null)
+    const [feedbackSent, setFeedbackSent] = useState(false);
 
+    function handLeRestartFeedback(){
+        setFeedbackSent(false);
+        setFeedbackType(null);
+    }
 
     return(
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 
         flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            <header>
-                <span className="text-xl leading-6"> Deixe seu Feedback</span>
-
-                <CloseButton/>
-            </header>
-
-       {!feedbackType ? (
-           <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
-       ):(
-           <FeedbackContentStep/>
-       )}
+            {feedbackSent ?(
+                <FeedBackSuccessStep onFeedbackRestartRequested={handLeRestartFeedback} />
+            ): (
+            <>
+                {!feedbackType ? (
+                    <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
+                    ):(
+                    <FeedbackContentStep 
+                    feedbackType={feedbackType} 
+                    onFeedbackRestartRequested={handLeRestartFeedback}
+                    onFeedbackSent = {()=> setFeedbackSent(true)}
+                    />
+                )}
+            </>
+            )}
+     
 
             <footer className="text-xs text-neutral-400">
                 Feito por <a className="underline underline-offset-2" href="https://rafaelsilveirax.github.io/RafSilveira/">RafSilveira</a>  🚀 
